fa08ab3e36fb90eaa08b98121293c05fceca8077
arceryz <t.j.m.vandervalk@proton.me>
2025-10-10 16:06:26 +0200

    Blokken selectie

--- TetrisTemplate/GameStates/PlayingState.cs
+++ TetrisTemplate/GameStates/PlayingState.cs
@@ -1,26 +1,49 @@
-﻿
+﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 class PlayingState: GameState
 {
 	TetrisGrid grid;
 	TetrisBlock block;
+
+	List<TetrisBlock> nextBlocks = new();
+
     public PlayingState()
 	{
 		block = new TetrisBlock(TetrisBlockData.GetRandom());
-		block.Position = new Vector2(250, 100);
         grid = new TetrisGrid();
         grid.Position = new Vector2(250, 0);
+
+		for (int i = 0; i < 5; i++)
+		{
+			nextBlocks.Add(new TetrisBlock(TetrisBlockData.GetRandom()));
+		}
     }
 
 	public override void Update(float delta)
 	{
+		if (TetrisGame.Input.KeyPressed(Keys.X))
+		{
+			block = nextBlocks[0];
+			nextBlocks.RemoveAt(0);
+			nextBlocks.Add(new TetrisBlock(TetrisBlockData.GetRandom()));
+		}
 	}
 
 	public override void Draw(SpriteBatch batch)
 	{
+		block.Position = new Vector2(250, 100);
+
 		grid.Draw(batch);
 		block.Draw(batch);
+
+		for (int i = 0; i < nextBlocks.Count; i++)
+		{
+			TetrisBlock block = nextBlocks[i];
+			block.Position = new Vector2(650, 100 + 100 * i);
+			block.Draw(batch);
+		}
     }
 }
